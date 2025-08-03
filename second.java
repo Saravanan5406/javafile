267f6a20a9244c443c50d20afa13c29a26ab59b3
Saravanan <saravanasaravana248024@gmail.com>
2025-08-03 17:14:35 +0530

    variable created

--- second.java
+++ second.java
@@ -1,7 +1,9 @@
 public class second{
     public static void main(String[] args) {
         int container =66;
+        String container1 = "john";
         System.out.println(container);
+        System.out.println(container1);
             }
             
 }
