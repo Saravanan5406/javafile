17c215d619d284abdb07e88edc806372a16f778e
Saravanan <saravanasaravana248024@gmail.com>
2025-08-03 16:38:25 +0530

    name changed

---
+++ firstprogram.java
@@ -0,0 +1,8 @@
+public class firstprogram{
+    public static void main(String[] args) {
+        // varriable declaration
+        int container =66;
+        System.out.println(container);
+            }
+            
+}
