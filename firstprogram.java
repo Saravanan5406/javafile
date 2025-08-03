46424b23f354d42c49085c457978cd5cc08805d4
Saravanan <saravanasaravana248024@gmail.com>
2025-08-03 17:01:41 +0530

    class created

--- firstprogram.java
+++
@@ -1,8 +0,0 @@
-public class firstprogram{
-    public static void main(String[] args) {
-        // varriable declaration
-        int container =66;
-        System.out.println(container);
-            }
-            
-}
