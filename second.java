4d942be72c41258866b01d52e3ca6a8be3c03629
Saravanan <saravanasaravana248024@gmail.com>
2025-08-03 17:32:53 +0530

    new file created

--- second.java
+++ second.java
@@ -1,9 +1,10 @@
 public class second{
     public static void main(String[] args) {
         int container =66;
-        String container1 = "john";
+        String john = "Happy Birthday ";       
+        String container1 = john;
         System.out.println(container);
         System.out.println(container1);
             }
             
-}
+}  
